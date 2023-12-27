ee52a7f7546f67f5b96900db89ca26415fcf6c4d
Ruaa <Ruaa@DESKTOP-RAEOIB2>
2023-12-27 19:31:30 +0200

    trying  paste

--- paste.h
+++ paste.h
@@ -1,9 +1,20 @@
 #pragma once
 #include "Actions/Action.h"
 #include "ApplicationManager.h"
+#include "Start.h"
 class paste :
     public Action
 {
+
+    Statement* copied;
+    Point position;
+
+public:
+    paste(ApplicationManager* pAppManager);
+
+    void ReadActionParameters();
+
+    void Execute();
 
 };
 
